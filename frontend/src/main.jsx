7d6c65438bba155fd756c5e0e6b0ada3af22288d
Abbas Anosh <abbasanosh123@gmail.com>
2024-07-23 21:10:54 -0600

    react router dom setup and clerk authentication setup done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "../src/pages/home/Home";
+import Dashboard from "../src/pages/dashboard/Dashboard";
+import Chat from "../src/pages/chat/Chat";
+import RootLayout from "./layouts/rootLayout/RootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUp />,
+      },
+
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <Chat />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- frontend/src/pages/signUp/SignUp.jsx
+++ frontend/src/pages/signUp/SignUp.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { SignUp } from "@clerk/clerk-react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <SignUp path="/sign-up" />
+    </div>
+  );
 };
 
 export default SignUp;
